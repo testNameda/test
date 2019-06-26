ff5a50820b6376414f2009216ed2adddd53717f7
DESKTOP-OFO6AQ1\asua <976378508@qq.com>
2019-06-26 20:29:36 +0800

    修复轮播图BUG

--- centent/js/index.js
+++ centent/js/index.js
@@ -2,17 +2,16 @@ var col = ["#951935", "#ffffff", "#c9a207", "#e8e8e8", "#e8e8e8", "#e0d1b8"];
 var img = document.getElementById("main-bg");
 var marbot = document.getElementById("mar-bot");
 var id = 1;
+var interval3;
 var interval1;
 var interval2;
 var interval0;
-var onoff = true;
 
 init();
 
 function init() {
-    if (onoff) {
-        interval0 = setInterval(b, 5000);
-    }
+    console.log(123);
+    interval0 = setInterval(b, 6000);
 }
 
 function b() {
@@ -20,8 +19,11 @@ function b() {
         id = id + 1;
     else
         id = 1;
+    console.log(456);
+    clearInterval(interval1);
+    clearInterval(interval2);
+    clearInterval(interval3);
     interval1 = setInterval(asd, 40);
-    onoff = false;
 
 }
 
@@ -31,17 +33,18 @@ function lbt(i) {
         return;
     }
     clearInterval(interval0);
-    onoff = true;
     init();
     id = i;
-    interval1 = setInterval(asd, 20);
+    interval3 = setInterval(asd, 20);
 }
 
 function asd() {
     var a = img.style.opacity;
-    if (a <= 0) {
+    if (a <= 0.1) {
         clearInterval(interval1);
+        clearInterval(interval3);
         interval2 = setInterval(dsa, 20);
+        return;
     } else {
         img.style.opacity = img.style.opacity - 0.05;
     }
@@ -49,18 +52,23 @@ function asd() {
 
 function dsa() {
     var a = img.style.opacity;
-    marbot.style.backgroundColor = col[id - 1];
-    img.src = "img/main-bg/lunbotu-" + id + ".jpg";
+    if (parseInt(img.src.split("lunbotu-")[1].split(".jpg")[0]) != id) {
+        marbot.style.backgroundColor = col[id - 1];
+        img.src = "img/main-bg/lunbotu-" + id + ".jpg";
+        console.log(789);
+
+    }
     if (a >= 1) {
         clearInterval(interval2);
         var kl;
         for (var s = 1; s < 7; s++) {
             kl = document.getElementById("lbt-" + s);
-            kl.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+            kl.style.backgroundColor = "#7c7c7c";
         }
         var lbt = document.getElementById("lbt-" + id);
         lbt.style.backgroundColor = "#ffffff";
+        return;
     } else {
-        img.style.opacity = (parseFloat(img.style.opacity) + 0.05) + "";
+        img.style.opacity = (parseFloat(img.style.opacity) + 0.02) + "";
     }
 }
